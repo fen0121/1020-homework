c3cb786e1af1d9265c731cadeb077d38fc1b09a3
fen0121 <71689172+fen0121@users.noreply.github.com>
2020-11-03 15:07:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,33 +2,14 @@ basic.forever(function () {
     for (let i = 0; i <= 4; i++) {
         for (let j = 0; j <= i; j++) {
             led.plot(4 - i + j, j)
-            basic.pause(100)
         }
+        basic.pause(100)
     }
     for (let i = 0; i <= 3; i++) {
-        for (let j = 0; j <= i; j++) {
-            led.plot(i, j + 1)
-            basic.pause(30)
+        for (let j = 0; j <= 4 - i; j++) {
+            led.plot(j, i + j + 1)
         }
-    }
-    for (let i = 0; i <= 3; i++) {
-        let index = 0
-        for (let j = 0; j <= index; j++) {
-            led.plot(i, j + 2)
-            basic.pause(30)
-        }
-    }
-    for (let i = 0; i <= 3; i++) {
-        for (let j = 0; j <= i; j++) {
-            led.plot(i, j + 3)
-            basic.pause(20)
-        }
-    }
-    for (let i = 0; i <= 3; i++) {
-        for (let j = 0; j <= i; j++) {
-            led.plot(i, j + 4)
-            basic.pause(20)
-        }
+        basic.pause(100)
     }
     basic.clearScreen()
 })
